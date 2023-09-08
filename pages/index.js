f2273ffbb6e9fc327e29fdf7aaf926adeefbfdb2
Andrea Lazari <64680733+andrealazari@users.noreply.github.com>
2023-09-08 01:44:38 +0000

    updating the text

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Olha o que você já conquistou até agora!! There is more to come!</h1>
+    return <h1>Olha o que você já conquistou até agora!! There is more to come! 💪</h1>
 }
 
 export default Home;
