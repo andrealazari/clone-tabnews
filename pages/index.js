7e92c8d2fd05518a2fa000dfa31a15bb8589a190
Andrea Lazari <64680733+andrealazari@users.noreply.github.com>
2023-09-17 11:38:25 +0000

    add scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Olha o que você já conquistou até agora!! There is more to come! 💪</h1>
+  return (
+    <h1>Olha o que você já conquistou até agora!! There is more to come! 💪</h1>
+  );
 }
 
 export default Home;
